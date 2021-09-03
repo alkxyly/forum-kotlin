77f4bfae7e12af8baaf42b25116a85ea16d3bfaa
alkxyly <alkxyly@hotmail.com>
2021-09-03 18:59:50 -0300

    02.06 Criando a camada Service

---
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -0,0 +1,28 @@
+package br.com.alura.forum.service
+
+import br.com.alura.forum.model.Curso
+import br.com.alura.forum.model.Topico
+import br.com.alura.forum.model.Usuario
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class TopicoService {
+
+    fun listar(): List<Topico> {
+        val topico = Topico(
+            id = 1,
+            titulo = "Duvida Kotlin",
+            mensagem = "Variaveis Kotlin",
+            curso = Curso(
+                id = 1,
+                nome = "Curso Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(id = 1, nome = "Ana", email = "ana@gmail.com"),
+
+            )
+        return Arrays.asList(topico, topico ,topico);
+    }
+
+}
